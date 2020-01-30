f3f02b09523fd326b20856c1e98acbdd6bbc1df6
FlorinM <florin.moraru@loveholidays.com>
2020-01-30 15:06:08 +0000

    First version with two modes

---
+++ entrypoint.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+
+SUPPORTED_MODES=(ENV_VAR IMAGE_TAG)
+MODE=$1
+CONTAINER_NAME=""
+NEW_IMAGE_TAG=""
+FILEPATH=""
+ENV_NAME=""
+NEW_ENV_VALUE=""
+
+if [[ ! " ${SUPPORTED_MODES[@]} " =~ " ${MODE} " ]]; then
+  echo " +++++++++ ERROR MODE \"${MODE}\" is not part of the supported values [ ${SUPPORTED_MODES[@]} ] " >&2
+  exit 1
+fi
+
+if [[ ${MODE} == "IMAGE_TAG" ]]; then
+  CONTAINER_NAME=$2
+  NEW_IMAGE_TAG=$3
+  FILEPATH=$4
+fi
+
+if [[ ${MODE} == "ENV_VAR" ]]; then
+  CONTAINER_NAME=$2
+  ENV_NAME=$3
+  NEW_ENV_VALUE=$4
+  FILEPATH=$5
+fi
+
+if test -f "${FILEPATH}"; then
+  echo " +++ + Updating file ${FILEPATH}"
+else
+ echo " +++++++++ ERROR file \"${FILEPATH}\" does not exist" >&2
+ exit 1
+fi
+
+
+if [[ ${MODE} == "IMAGE_TAG" ]]; then
+  SUPPORTED_OBJECT_KINDS=(Deployment StatefulSet CronJob Kustomization)
+  objectKind=$(yq r ${FILEPATH} kind)
+  echo " +++ + Detected Object kind as \"${objectKind}\" "
+
+  if [[ ! " ${SUPPORTED_OBJECT_KINDS[@]} " =~ " ${objectKind} " ]]; then
+    echo " +++++++++ ERROR Object kind \"${objectKind}\" is not part of the supported values [ ${SUPPORTED_OBJECT_KINDS[@]} ] for file ${FILEPATH} " >&2
+    exit 1
+  fi
+
+  if [[ ${objectKind} == "Deployment" ]] || [[ ${objectKind} == "StatefulSet" ]] ; then
+      containerPosition=$(yq r ${FILEPATH} spec.template.spec.containers.*.name | grep -n ${CONTAINER_NAME}$ | cut -d: -f1)
+      containerIndex=$((${containerPosition/M/}-1))
+      if (( ${containerIndex} < 0 )); then
+        echo " +++++++++ ERROR container with name ${CONTAINER_NAME} could not be found in file  ${FILEPATH}" >&2
+        exit 1
+      fi
+
+      echo " +++ + Container Index $containerIndex"
+      currentImageValue=$(yq r ${FILEPATH} spec.template.spec.containers[${containerIndex}].image)
+      ocurrancesCount=$(grep -o "$currentImageValue" ${FILEPATH} | wc -l)
+      if (( ${ocurrancesCount} > 1 )); then
+          echo " +++++++++ ERROR Cannot update file ${FILEPATH}  as there are multiple occurrences of tag ${currentImageValue}" >&2
+          exit 1
+      fi
+      echo " +++ + + Processing image from $currentImageValue"
+
+      imageFullName=$(grep -Po '\K.*?(?=:)' <<< ${currentImageValue})
+      if [ -z "${imageFullName}" ]; then imageFullName=${currentImageValue}; fi
+      echo " +++ + + to new  image  tag    ${imageFullName}:${NEW_IMAGE_TAG}"
+      sed -i "s+${currentImageValue}+${imageFullName}:${NEW_IMAGE_TAG}+g" ${FILEPATH}
+  fi
+
+  if [[ ${objectKind} == "CronJob" ]] ; then
+      containerPosition=$(yq r ${FILEPATH} spec.jobTemplate.spec.template.spec.containers.*.name | grep -n ${CONTAINER_NAME}$ | cut -d: -f1)
+      containerIndex=$((${containerPosition/M/}-1))
+      if (( ${containerIndex} < 0 )); then
+        echo " +++++++++ ERROR container with name ${CONTAINER_NAME} could not be found in file CronJob  ${FILEPATH}" >&2
+        exit 1
+      fi
+
+      echo " +++ + Container Index in CronJob $containerIndex"
+      currentImageValue=$(yq r ${FILEPATH} spec.jobTemplate.spec.template.spec.containers[${containerIndex}].image)
+      ocurrancesCount=$(grep -o "$currentImageValue" ${FILEPATH} | wc -l)
+      if (( ${ocurrancesCount} > 1 )); then
+          echo " +++++++++ ERROR Cannot update file ${FILEPATH}  as there are multiple occurrences of tag ${currentImageValue}" >&2
+          exit 1
+      fi
+      echo " +++ + + Processing image from $currentImageValue"
+
+      imageFullName=$(grep -Po '\K.*?(?=:)' <<< ${currentImageValue})
+      if [ -z "${imageFullName}" ]; then imageFullName=${currentImageValue}; fi
+      echo " +++ + + to new  image  tag    ${imageFullName}:${NEW_IMAGE_TAG}"
+      sed -i "s+${currentImageValue}+${imageFullName}:${NEW_IMAGE_TAG}+g" ${FILEPATH}
+  fi
+
+
+  if [[ ${objectKind} == "Kustomization" ]] ; then
+    kustomizeBuildPath="${FILEPATH%/*}"
+    echo " +++ + Building kustomize in directory ${kustomizeBuildPath}"
+    fullKustomizeBuild=$(kustomize build ${kustomizeBuildPath})
+
+    delimiter="---"
+    s=$fullKustomizeBuild$delimiter
+    kustomizeImageNameToUpdate=""
+    while [[ $s ]]; do
+        object="${s%%"$delimiter"*}"
+        containerPosition=$(echo "$object" | yq r - spec.template.spec.containers.*.name | grep -n ${CONTAINER_NAME}$ | cut -d: -f1)
+        if [[ $containerPosition ]]; then
+          containerIndex=$((${containerPosition/M/}-1))
+          currentImageValue=$(echo "$object" | yq r - spec.template.spec.containers[${containerIndex}].image)
+          if [[ ! $currentImageValue ]]; then
+            currentImageValue=$(echo "$object" | yq r - spec.jobTemplate.spec.template.spec.containers[${containerIndex}].image)
+          fi
+          imageFullName=$(grep -Po '\K.*?(?=:)' <<< ${currentImageValue})
+          if [ -z "${imageFullName}" ]; then imageFullName=${currentImageValue}; fi
+          kustomizeImageNameToUpdate=${imageFullName}
+        fi
+        s=${s#*"$delimiter"};
+    done;
+
+    if [[ ! $kustomizeImageNameToUpdate ]]; then
+      echo " +++++++++ ERROR container with name ${CONTAINER_NAME} could not be found in any file build by kustomize from folder ${kustomizeBuildPath}" >&2
+      exit 1
+    fi
+
+    kustomizeImageNamePosition=$(yq r ${FILEPATH} images.*.name | grep -n ${kustomizeImageNameToUpdate} | cut -d: -f1)
+    kustomizeContainerIndex=$((${kustomizeImageNamePosition/M/}-1))
+    kustomizeCurrentNewTagValue=$(yq r ${FILEPATH} images[${kustomizeContainerIndex}].newTag)
+    ocurrancesCount=$(grep -o "$kustomizeCurrentNewTagValue" ${FILEPATH} | wc -l)
+    if (( ${ocurrancesCount} > 1 )); then
+        echo " +++++++++ ERROR Cannot update file ${FILEPATH}  as there are multiple occurrences of string ${kustomizeCurrentNewTagValue}" >&2
+        exit 1
+    fi
+    echo " +++ + + Processing newTag for image name: $kustomizeImageNameToUpdate"
+    echo " +++ + + + from newTag: ${kustomizeCurrentNewTagValue}"
+    echo " +++ + + + to   newTag: ${NEW_IMAGE_TAG}"
+    sed -i "s+${kustomizeCurrentNewTagValue}+${NEW_IMAGE_TAG}+g" ${FILEPATH}
+  fi
+fi
+
+if [[ ${MODE} == "ENV_VAR" ]]; then
+  SUPPORTED_OBJECT_KINDS=(Deployment StatefulSet)
+  objectKind=$(yq r ${FILEPATH} kind)
+  echo " +++ + Detected Object kind as \"${objectKind}\" "
+
+  if [[ ! " ${SUPPORTED_OBJECT_KINDS[@]} " =~ " ${objectKind} " ]]; then
+    echo " +++++++++ ERROR Object kind \"${objectKind}\" is not part of the supported values [ ${SUPPORTED_OBJECT_KINDS[@]} ] for file ${FILEPATH} " >&2
+    exit 1
+  fi
+
+  if [[ ${objectKind} == "Deployment" ]] || [[ ${objectKind} == "StatefulSet" ]] ; then
+    containerPosition=$(yq r ${FILEPATH} spec.template.spec.containers.*.name | grep -n ${CONTAINER_NAME}$ | cut -d: -f1)
+    containerIndex=$((${containerPosition/M/}-1))
+    if (( ${containerIndex} < 0 )); then
+      echo " +++++++++ ERROR container with name ${CONTAINER_NAME} could not be found in file  ${FILEPATH}" >&2
+      exit 1
+    fi
+
+    echo " +++ + Container Index $containerIndex"
+    envPosition=$(yq r ${FILEPATH} spec.template.spec.containers[${containerIndex}].env[*].name | grep -n ${ENV_NAME}$ | cut -d: -f1)
+    envIndex=$((${envPosition/M/}-1))
+    if (( ${envIndex} < 0 )); then
+      echo " +++++++++ ERROR Environment variable with name ${ENV_NAME} not found in ${CONTAINER_NAME}" >&2
+      exit 1
+    fi
+    currentEnvValue=$(yq r ${FILEPATH} spec.template.spec.containers[${containerIndex}].env[${envIndex}].value)
+    ocurrancesCount=$(grep -o "$currentEnvValue" ${FILEPATH} | wc -l)
+    if (( ${ocurrancesCount} > 1 )); then
+        echo " +++++++++ ERROR Cannot update file ${FILEPATH}  as there are multiple occurrences of string ${currentEnvValue}" >&2
+        exit 1
+    fi
+    echo " +++ + + Updating ${ENV_NAME} in container ${CONTAINER_NAME} from ${currentEnvValue}"
+    echo " +++ + + To env   ${ENV_NAME} in container ${CONTAINER_NAME} to   ${NEW_ENV_VALUE}"
+    sanitizedOldString=$(echo $currentEnvValue | sed 's/[][`~!@#$%^&*()-+{}\|;:_=",<.>/?'"'"']/\\&/g')
+    sanitizedNewString=$(echo $NEW_ENV_VALUE | sed 's/[][`~!@#$%^&*()-+{}\|;:_=",<.>/?'"'"']/\\&/g')
+    echo "sed -i s+${sanitizedOldString}+${sanitizedNewString}+g ${FILEPATH}"
+    sed -i "s+${sanitizedOldString}+${sanitizedNewString}+g" ${FILEPATH}
+  fi
+fi
